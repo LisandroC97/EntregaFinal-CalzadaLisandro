db608714200a61ac35f0a490850e99afa3bd3e42
Lisandro Calzada <clisandro1997@gmail.com>
2023-04-14 18:10:27 -0300

    main

---
+++ Labs/Untitled-1.py
@@ -0,0 +1,5 @@
+for (let i = 1; i <= 100; i++) {
+  const imgUrl = `../Images/fotos/WhatsApp Image 2023-04-14 at 17.53.11 (${i}).jpeg`;
+  const imgHtml = `<img src="${imgUrl}">`;
+  console.log(imgHtml); // Opcionalmente, se puede imprimir en la consola del navegador o en la consola de Node.js
+}
---
+++ Pages/Untitled-1.py
@@ -0,0 +1,2 @@
+for i in range(1, 101):
+    print('<img class="image-fluid galeria" src="../Images/fotos/WhatsApp Image 2023-04-14 at 17.53.11 (' + str(i) + ').jpeg">')
